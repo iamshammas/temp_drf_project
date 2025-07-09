7f65310c52dcf2a28d8537d5a53f8bc1b00a9eea
SHAMMAS <muhdshammas612@gmail.com>
2025-07-10 00:51:15 +0530

    EmployeeModel added and migrations done

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class employeeModel(models.Model):
+    emp_id = models.IntegerField()
+    emp_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.emp_name
+    
