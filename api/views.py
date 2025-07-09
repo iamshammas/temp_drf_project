bad8845282583c9cbf19a823ec7e74bf1ee231e0
SHAMMAS <muhdshammas612@gmail.com>
2025-07-09 23:29:43 +0530

    POST request successfully created

--- api/views.py
+++ api/views.py
@@ -8,9 +8,15 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 
-@api_view (['GET'])
+@api_view (['GET','POST'])
 def studentsView(request):
     if request.method == 'GET':
         students = studentModel.objects.all()
         serializer = studentSerializer(students,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = studentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
