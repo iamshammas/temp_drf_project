6b679b56c096a217c09ac8c89fa30ca41b033475
SHAMMAS <muhdshammas612@gmail.com>
2025-07-14 01:38:16 +0530

    put method added using drf

--- api/views.py
+++ api/views.py
@@ -63,3 +63,9 @@ class employeeDetailView(APIView):
     def get(self,request,pk):
         serializer = employeeSerializer(self.get_data(pk))
         return Response(serializer.data,status=status.HTTP_200_OK)
+    def put(self,request,pk):
+        serializer = employeeSerializer(self.get_data(pk),request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
